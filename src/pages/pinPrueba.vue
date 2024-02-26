2e52f8f7694ff1a967baf19236f165a054f3ba34
AitorRD <aitroddue@alum.us.es>
2024-02-26 20:42:29 +0100

    feat(component): new pin component

---
+++ src/pages/pinPrueba.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="centered-image">
+   <slot />
+   <Pin />
+   </div>
+ </template>
+ 
+ <script setup lang="ts">
+ import Pin from '@/components/Pin.vue';
+ </script>
+ 
+ 
+ <style scoped>
+ .centered-image {
+   display: flex;
+   justify-content: center;
+   align-items: center;
+   width: 100vw;
+   height: 100vh;
+ }
+ 
+ </style>
