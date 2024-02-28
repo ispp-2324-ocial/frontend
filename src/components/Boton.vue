60261fa114e9fe271ae741e3b75d0afa12519018
Ramon J. Guerrero Romero <ramonguerom02@gmail.com>
2024-02-28 17:12:51 +0100

    feat(boton): realizado boton
    realizado el componenete de boton con icono

---
+++ src/components/Boton.vue
@@ -0,0 +1,35 @@
+<template>
+  <div
+    class="pin-container"
+    @mouseover="isHovered = true"
+    @mouseout="isHovered = false">
+    <button
+      class="checkbox-class"
+      :class="{ 'scaled': isHovered }"
+      @click="handleClick">
+      <CarbonFaceCool class="text-yellow text-3xl" />Texto
+    </button>
+  </div>
+</template>
+
+
+<script>
+import { ref } from 'vue';
+
+export default {
+  methods: {
+    handleClick() {
+      console.log('Button clicked!');
+    }
+  }
+};
+const isHovered = ref(true);
+</script>
+
+<style scoped>
+  .scaled {
+    cursor: pointer;
+    transform: scale(0.9);
+    transition: transform 0.3s ease;
+  }
+  </style>
