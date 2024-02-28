8614483e569b75c23d22381a9cbda5f414f4abff
jeszamgue <jeszamgue@alum.us.es>
2024-02-28 13:41:16 +0100

    feat(checkbox): creacion inicial checkbox

---
+++ src/pages/checboxPrueba.vue
@@ -0,0 +1,42 @@
+<template>
+    <p>
+      Using the custom checkbox with an array
+    </p>
+    <p>{{ selectedPeople }}</p>
+    <template v-for="person in people" key="person.name" >
+    <Checkbox :label="person.name" v-model="selectedPeople" :value="person" />
+    </template>
+  
+   <br> 
+   <p>  Using the custom checkbox as stand alone </p>
+   <Checkbox  :label="`I am checked: ${isSelected}`" v-model="isSelected"  />
+  </template>
+  
+  <script setup lang="ts">
+  import { ref } from 'vue'
+  const people = ref([
+    {
+      name:"William",
+      age: 30,
+      sex: "Male"
+    },
+    {
+      name:"Thomas",
+      age: 24,
+      sex: "Male"
+    },
+     {
+      name:"Jennifer",
+      age: 20,
+      sex: "Female"
+    }
+  ]);
+  
+  const selectedPeople = ref([]);
+  const isSelected= ref(false);
+  </script>
+
+
+ <style scoped>
+ 
+ </style>
