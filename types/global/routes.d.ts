8b6988cd37a0eeb8126ac7fbceb2f96d953cba8f
jeszamgue <jeszamgue@alum.us.es>
2024-02-28 13:41:16 +0100

    feat(checkbox): creacion inicial checkbox

--- types/global/routes.d.ts
+++ types/global/routes.d.ts
@@ -19,6 +19,7 @@ declare module 'vue-router/auto-routes' {
    */
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
+    '/checboxPrueba': RouteRecordInfo<'/checboxPrueba', '/checboxPrueba', Record<never, never>, Record<never, never>>,
     '/login/': RouteRecordInfo<'/login/', '/login', Record<never, never>, Record<never, never>>,
     '/pinPrueba': RouteRecordInfo<'/pinPrueba', '/pinPrueba', Record<never, never>, Record<never, never>>,
     '/prueba': RouteRecordInfo<'/prueba', '/prueba', Record<never, never>, Record<never, never>>,
