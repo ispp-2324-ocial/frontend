1903429b8f9f3b91be2c2bef4a06e6b7e563dfef
jeszamgue <jeszamgue@alum.us.es>
2024-03-01 16:57:55 +0100

    chore(checkbox): arreglado error lint

--- src/components/checkbox.vue
+++ src/components/checkbox.vue
@@ -1,23 +1,26 @@
 <template>
-    <div class="checkbox-container"
-       @mouseover="isHovered = true"
-       @mouseout="isHovered = false"
-    >
-    <input type="checkbox" v-model="value" :class="{ 'scaled': isHovered }"  />
-      <span> 
-        <slot />
-      </span>
-    </div>
-  </template>
-  
-<script setup lang="ts">
-import { ref } from "vue";
+  <div
+    class="checkbox-container"
+    @mouseover="isHovered = true"
+    @mouseout="isHovered = false">
+    <input
+      v-model="value"
+      type="checkbox"
+      :class="{ 'scaled': isHovered }" />
+    <span>
+      <slot />
+    </span>
+  </div>
+</template>
 
-const value = defineModel<Boolean>()
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const value = defineModel<boolean>();
 
 const isHovered = ref(false);
 </script>
-  
+
   <style scoped>
   .scaled {
     transform: scale(0.9);
