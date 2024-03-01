3f4f9a5e0a4557c7d5aff338ed910acc6e810657
AitorRD <aitroddue@alum.us.es>
2024-03-01 14:54:49 +0100

    fix(map): arreglo de container del mapa

--- src/pages/map.vue
+++ src/pages/map.vue
@@ -1,7 +1,14 @@
 <template>
-  <LMap />
+  <LMap :markers="markers"/>
 </template>
 
 <script setup lang="ts">
-
+  const markers = [
+    {
+      description: 'Concierto Nuevo Berlin',
+      latitud: 37.40869627313789,
+      longitud: -5.99000733820976,
+      category: 'Música',
+    }
+  ];
 </script>
