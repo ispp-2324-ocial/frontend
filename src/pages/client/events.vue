5355fb550565d6362d1d4e7651804a36c68fab34
adrrf <adrromflo@alum.us.es>
2024-03-01 16:55:04 +0100

    chore(client): added layout

--- src/pages/client/events.vue
+++ src/pages/client/events.vue
@@ -1,34 +1,8 @@
 <template>
-    <h1>Eventos</h1>
+  <h1>Eventos</h1>
 </template>
-  
-  <script setup lang="ts">
 
-  </script>
-  
-  <style>
-  #app {
-    display: flex;
-    flex-direction: column;
-    justify-content: space-around;
-    align-items: center;
-    gap: 1rem;
-    background-color: #f0f0f0;
-    padding: 20px;
-  }
-  
-  .card {
-    width: 250px;
-    background-color: white;
-    border: 1px solid #ccc;
-    border-radius: 10px;
-    padding: 20px;
-    cursor: pointer;
-    transition: background-color 0.3s ease-in-out;
-  }
-  
-  .card:hover {
-    background-color: #eee;
-  }
-  </style>
-  
+<script setup lang="ts">
+</script>
+<style scoped>
+</style>
