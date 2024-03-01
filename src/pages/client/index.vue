893bca948785a18a25148b1dbba51f0ddcedff60
adrrf <adrromflo@alum.us.es>
2024-03-01 16:55:04 +0100

    chore(client): added layout

--- src/pages/client/index.vue
+++ src/pages/client/index.vue
@@ -1,34 +1,45 @@
 <template>
-  <div class="flex flex-col justify-around items-center gap-4 bg-gray-200 p-5">
-    <div class="card" @click="redirectTo('/client/events')">
-      <h2>Tus Eventos</h2>
-      <p>Lista todos tus eventos</p>
-    </div>
+  <FooterComponent>
+    <div class="flex flex-col justify-around items-center gap-4 bg-gray-200 p-5">
+      <div
+        class="card"
+        @click="redirectTo('/client/events')">
+        <h2>Tus Eventos</h2>
+        <p>Lista todos tus eventos</p>
+      </div>
 
-    <div class="card" @click="redirectTo('/client/createEvent')">
-      <h2>Crear Eventos</h2>
-      <p>Crear un nuevo evento</p>
-    </div>
+      <div
+        class="card"
+        @click="redirectTo('/client/createEvent')">
+        <h2>Crear Eventos</h2>
+        <p>Crear un nuevo evento</p>
+      </div>
 
-    <div class="card" @click="redirectTo('/client/profile')">
-      <h2>Tu perfil</h2>
-      <p>Consulta todos los detalles de tu perfil</p>
+      <div
+        class="card"
+        @click="redirectTo('/client/profile')">
+        <h2>Tu perfil</h2>
+        <p>Consulta todos los detalles de tu perfil</p>
+      </div>
     </div>
-  </div>
+  </FooterComponent>
 </template>
 
 <script setup lang="ts">
+import FooterComponent from '@/layouts/default.vue';
 import { useRouter } from 'vue-router';
 
 const router = useRouter();
 
+/**
+ *
+ */
 function redirectTo(path: string) {
   router.push(path);
 }
 </script>
 
-<style>
-
+<style scoped>
 .card {
   width: 250px;
   background-color: white;
