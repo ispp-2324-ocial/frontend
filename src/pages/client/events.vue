69883492889e69260aaaa7a08e4faa75761c0d9a
horgarler <horgarler@alum.us.es>
2024-03-02 21:30:30 +0100

    feat(client): added events views

--- src/pages/client/events.vue
+++ src/pages/client/events.vue
@@ -1,8 +1,107 @@
 <template>
-  <h1>Eventos</h1>
+  <div>
+    <ul
+      class="-ml-7"
+      style="list-style-type:none;">
+      <li
+        v-for="(item, index) in eventDetail"
+        :key="index">
+        <div class="row mb-7 mr-3">
+          <div style="margin:auto; text-align: center;display: flex; align-items: center; justify-content: center;">
+            <div
+              class="ml-3"
+              style="text-align: left; width: 100%;">
+              <p style="text-align: center;font-size: clamp(15px, 7vw, 40px); font-weight: bold; justify-content: center;">
+                {{ item.name }}
+              </p>
+              <img
+                alt="Event image"
+                src="@/assets/images/temp/yoga.png"
+                class="center"
+                style="display: block; width: 30%; border-radius: 50%; max-width: 220px; max-height: 220px; min-width: 90px; min-height: 90px" />
+              <p style="font-size: clamp(15px, 6vw, 30px); font-weight: bold">
+                {{ item.event }}
+              </p>
+              <p style="font-size: clamp(15px, 6vw, 30px); font-weight: normal">
+                <b>Lugar:</b> {{ item.place }}
+              </p>
+              <p style="font-size: clamp(15px, 6vw, 30px); font-weight: normal">
+                <b>Fecha: </b>{{ item.date }} a las {{ item.hour }}
+              </p>
+              <p style="font-size: clamp(15px, 6vw, 30px); font-weight: normal">
+                <b>Capacidad:</b> {{ item.capacity }}
+              </p>
+              <p style="font-size: clamp(15px, 6vw, 30px); font-weight: normal">
+                <b>Categoría:</b> {{ item.category }}
+              </p>
+            </div>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
+  <div
+    class="mb-7"
+    style="background-color: #deecff; width: 100%;">
+    <div
+      style="justify-content: center; display: flex;"
+      @click="redirectTo('/client/subscription')">
+      <Boton class="ocial-button">
+        <div>
+          <p style="color:#0e4791; font-size: 60%;">
+            Comprobar suscripciones
+          </p>
+        </div>
+      </Boton>
+    </div>
+  </div>
 </template>
 
+
 <script setup lang="ts">
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+/**
+ *
+ */
+function redirectTo(path: string) {
+  router.push(path);
+}
+
+const eventDetail = [
+  {
+    name: 'Yoga Outdoor',
+    place: 'Parque del Alamillo',
+    event: 'Clase de yoga al aire libre.',
+    date: '16/03/24',
+    hour: '16:00',
+    capacity: 60,
+    category: 'Deporte',
+    image: '@/assets/images/temp/yoga.png'
+  }
+];
+
 </script>
+
 <style scoped>
+.center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+}
+.ocial-button {
+  text-transform: uppercase;
+  background: #aacfff;
+  width: 80%;
+  border: 2px solid #3e80d7;
+  border-radius: 20px;
+  font-size: 200%;
+  font-weight: bold;
+  margin: auto;
+  text-align: center;
+  justify-content: center;
+}
 </style>
--- src/pages/client/index.vue
+++ src/pages/client/index.vue
@@ -16,8 +16,8 @@
 
     <div>
       <h1
-        class="ml-8"
-        style="font-size: 24px;">
+        class="ml-8 mb-1"
+        style="font-size: 7vw;">
         Tus eventos
       </h1>
       <ul
@@ -26,23 +26,32 @@
         <li
           v-for="(item, index) in tempEvents"
           :key="index">
-          <div class="row mb-7 mr-3">
-            <div style="margin:auto; text-align: center;display: flex; align-items: center; justify-content: left;">
+          <div
+            class="row mr-3"
+            style="border-top: solid 1px #b0b0b0; padding: 0%;"
+            @click="redirectTo('/client/events')">
+            <div style=" cursor: pointer;margin:auto; text-align: center;display: flex; align-items: center; justify-content: left;">
+              <!-- src se tiene que cambiar por v-bind:src="image" cuando se junte con back-->
               <img
                 alt="Event image"
                 src="@/assets/images/temp/yoga.png"
-                style="display: block; width: 30%; border-radius: 50%;" />
+                style="display: block; width: 30%; border-radius: 50%; max-width: 220px; max-height: 220px; min-width: 90px; min-height: 90px; margin: 16px;" />
               <div
                 class="ml-3"
                 style="text-align: left; width: 70%;">
-                <p style="font-size: 6vw; font-weight: bold">
-                  {{ item.name }}
-                </p>
+                <div>
+                  <p style="font-size: clamp(15px, 6vw, 30px); font-weight: bold; margin-bottom: 0px;">
+                    {{ item.name }}
+                  </p>
+                </div>
+
                 <p
-                  style="font-size: 5vw;">
+                  style="font-size: clamp(15px, 5vw, 20px); margin-top: 3px; margin-bottom: 0px;">
                   {{ item.date }} a las {{ item.hour }}
                 </p>
-                <p>En {{ item.place }}</p>
+                <p style="font-size: clamp(14px, 3vw, 18px); margin-top: 3px;">
+                  En {{ item.place }}
+                </p>
               </div>
             </div>
             <div />
@@ -59,39 +68,24 @@ const tempEvents = [
   {
     name: 'Yoga Outdoor',
     place: place1,
-    event: 'Clase de yoga al aire libre.',
-    date: '2024-03-16',
-    hour: '16:00:00',
-    capacity: 60,
+    date: '16/03/24',
+    hour: '16:00',
     category: 'Category.SPORTS',
-    latitude: 37.418_594,
-    longitude: -5.996_096,
-    ocialClient: 'Alamillo',
     image: '@/assets/images/temp/yoga.png'
   },
   {
     name: 'Competición de calistenia',
     place: place1,
-    event: 'Competición deportiva de calistenia en el parque de barras del Alamillo. Se requiere de inscripción previa.',
-    date: '2024-03-24',
-    hour: '17:30:00',
-    capacity: 40,
+    date: '24/03/24',
+    hour: '17:30',
     category: 'Category.SPORTS',
-    latitude: 37.415_472_529_948_86,
-    longitude: -5.998_031_476_148_601,
-    ocialClient: 'Alamillo',
     image: '@/assets/images/temp/barras.png'
   }, {
     name: 'In the Park',
     place: place1,
-    event: 'Conciertos al aire libre.',
-    date: '2024-03-30',
-    hour: '11:00:00',
-    capacity: 800,
+    date: '30/03/24',
+    hour: '11:00',
     category: 'Category.MUSIC',
-    latitude: 37.418_251_778_866_25,
-    longitude: -5.996_513_552_310_005,
-    ocialClient: 'Alamillo',
     image: '@/assets/images/temp/inthepark.png'
   }
 ];
