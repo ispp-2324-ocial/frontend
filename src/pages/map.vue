7d200117ffb2d50f05b86713c843a1ece0383360
AitorRD <aitroddue@alum.us.es>
2024-03-04 22:06:08 +0100

    chore(map): añadidos markers

--- src/pages/map.vue
+++ src/pages/map.vue
@@ -9,6 +9,16 @@ const markers = [
     latitud: 37.408_696_273_137_89,
     longitud: -5.990_007_338_209_76,
     category: 'Música'
+  },{
+    description: 'Clase Yoga en Alamillo',
+    latitud: 37.4154742332577,
+    longitud: -5.997796824409255,
+    category: 'Deporte'
+  },{
+    description: 'Partido Fútbol Casa Félix',
+    latitud: 37.35830745786976, 
+    longitud: -5.98612527513324,
+    category: 'Deporte'
   }
 ];
 </script>
