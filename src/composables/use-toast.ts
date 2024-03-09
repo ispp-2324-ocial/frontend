e2d172595868ded07bb8b935e0109dd79c921353
paupenfer1 <paaulap02@gmail.com>
2024-03-09 13:22:05 +0100

    feat/35: Notificador

--- src/composables/use-toast.ts
+++ src/composables/use-toast.ts
@@ -1,34 +1,38 @@
 import { toast } from 'vue3-toastify';
 
+type NotificationTypes = 'warning' | 'success' | 'error' | 'info';
 
-export const notify = (type: string) => {
+
+export const notify = (message?:string, type?: NotificationTypes): void => {
   switch (type) {
-    case 'default':
-      toast("Default Notification !");
-      break;
-    case 'success':
-      toast.success("Success Notification !", {
-        position: toast.POSITION.TOP_CENTER,
+    case 'success': {
+      toast.success('Success Notification !' || message, {
+        position: toast.POSITION.TOP_CENTER
       });
       break;
-    case 'error':
-      toast.error("Error Notification !", {
-        position: toast.POSITION.TOP_LEFT,
+    }
+    case 'error': {
+      toast.error('Error Notification !'|| message, {
+        position: toast.POSITION.TOP_LEFT
       });
       break;
-    case 'warning':
-      toast.warn("Warning Notification !", {
-        position: toast.POSITION.BOTTOM_LEFT,
+    }
+    case 'warning': {
+      toast.warn('Warning Notification !'|| message, {
+        position: toast.POSITION.BOTTOM_LEFT
       });
       break;
-    case 'info':
-      toast.info("Info Notification !", {
-        position: toast.POSITION.BOTTOM_CENTER,
+    }
+    case 'info': {
+      toast.info('Info Notification !'|| message, {
+        position: toast.POSITION.BOTTOM_CENTER
       });
       break;
-    default:
-      toast("Custom Style Notification with css class!", {
-        position: toast.POSITION.BOTTOM_RIGHT,
+    }
+    default: {
+      toast('Notification'|| message, {
+        position: toast.POSITION.BOTTOM_RIGHT
       });
+    }
   }
 };
