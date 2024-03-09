ffacf0b602ab8a98ce5b96bc0533cab8aa8dbc7d
paupenfer1 <paaulap02@gmail.com>
2024-03-09 18:16:15 +0100

    fix:traduccion

--- src/composables/use-toast.ts
+++ src/composables/use-toast.ts
@@ -1,4 +1,8 @@
 import { toast } from 'vue3-toastify';
+import { useI18n } from 'vue-i18n';
+
+const { t } = useI18n();
+
 
 type NotificationTypes = 'warning' | 'success' | 'error' | 'info';
 
@@ -6,31 +10,31 @@ type NotificationTypes = 'warning' | 'success' | 'error' | 'info';
 export const notify = (message?:string, type?: NotificationTypes): void => {
   switch (type) {
     case 'success': {
-      toast.success(message ?? 'Success Notification !' , {
+      toast.success(message ?? t('Success Notification !') , {
         position: toast.POSITION.TOP_CENTER
       });
       break;
     }
     case 'error': {
-      toast.error(message ?? 'Error Notification !', {
+      toast.error(message ?? t('Error Notification !'), {
         position: toast.POSITION.TOP_LEFT
       });
       break;
     }
     case 'warning': {
-      toast.warn(message ?? 'Warning Notification !', {
+      toast.warn(message ?? t('Warning Notification !'), {
         position: toast.POSITION.BOTTOM_LEFT
       });
       break;
     }
     case 'info': {
-      toast.info(message ?? 'Info Notification !', {
+      toast.info(message ?? t('Info Notification !'), {
         position: toast.POSITION.BOTTOM_CENTER
       });
       break;
     }
     default: {
-      toast(message ?? 'Notification', {
+      toast(message ?? t('Notification'), {
         position: toast.POSITION.BOTTOM_RIGHT
       });
     }
