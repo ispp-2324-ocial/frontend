bf40670d3ef3c0f8151f05f092512cc6da48a3f0
Ramon J. Guerrero Romero <ramonguerom02@gmail.com>
2024-03-16 22:03:58 +0100

    feat(buscador): integrado API

--- src/pages/pruebaBuscador.vue
+++ src/pages/pruebaBuscador.vue
@@ -2,12 +2,15 @@
   <div
     class="centered-image"
     style="margin-top: 10%;">
-    <BaseInput v-model="firstName" />
-    <Boton
-      class="transparente"
-      @click="getEvents(firstName)">
-      <IIwwaSearch class="size-6 text-muted-foreground" />
-    </Boton>
+    <form @submit.prevent="()=>{}">
+      <BaseInput v-model="firstName" />
+      <Boton
+        class="transparente"
+        @click="getEvents(firstName)">
+        <IIwwaSearch class="size-6 text-muted-foreground" />
+      </Boton>
+    </form>
+    {{ miArray }}
   </div>
 </template>
 
@@ -18,7 +21,7 @@ import { useEvent } from '@/composables/apis';
 
 const firstName = ref('');
 const { data: eventList } = await useEvent(EventApi, 'eventListList')();
-const miArray: Event[] = [];
+const miArray = ref<Event[]>([]);
 
 console.log(eventList.value);
 
@@ -26,9 +29,11 @@ console.log(eventList.value);
  * Prueba para buscar
  */
 function getEvents(text: string): void {
+  miArray.value.length = 0;
+
   for (const event of eventList.value) {
-    if (event.name.includes(text) || event.event.includes(text) || event.ocialClient.toString().includes(text)){
-      miArray.push(event);
+    if (event.name.includes(text) || event.event.includes(text) || String(event.ocialClient).includes(text)){
+      miArray.value.push(event);
     }
   }
 
