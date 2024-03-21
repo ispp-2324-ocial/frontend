64a9c970f29878ced09cfaf58616c2b93ecb4311
carvarsou <carvarsou@alum.us.es>
2024-03-21 15:09:43 +0100

    fix(filter): rework on slider component
    - changed the styles
    - changed the template and script
    
    squash
    
    Signed-off-by: GitHub <noreply@github.com>

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,84 +1,118 @@
 <template>
   <div class="slider-component">
-    <div class="slider-container">
-      <input
-        ref="input"
-        v-model="currentValue"
-        type="range"
-        :min="min"
-        :max="max"
-        class="slider"
-        @input="slide" />
+    <input
+      v-model="value"
+      type="range"
+      :min="min"
+      :max="max"
+      :step="step"
+      class="slider"
+      @input="showCurrentValue"
+      @change="hideCurrentValue" />
+
+    <div
+      v-if="showValue"
+      class="current-value">
+      <p>{{ value }} {{ $t(' km') }} </p>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 
-const props = defineProps({
-  value: {
-    type: Number,
-    required: true
-  },
-  min: {
-    type: Number,
-    required: true
-  },
-  max: {
-    type: Number,
-    required: true
-  }
-});
+withDefaults(defineProps<{
+  min: number;
+  max: number;
+  step: number;
+}>(), {
+  min: 0,
+  max: 1000,
+  step: 10}
+);
 
-const emits = defineEmits(['input']);
-const currentValue = defineModel<string>();
+const value = defineModel<string>();
+const showValue = ref(false);
 
-const data = (): number => {
-  return props.value;
-};
+/**
+ * Show current value on slider when moving the slider thumb
+ */
+function showCurrentValue(): void {
+  showValue.value = true;
+}
 
-const slide = (): void => {
-  emits('input', Number.parseInt(data().toString()));
-};
+/**
+ * Hide current value on slider when restarting the page
+ */
+function hideCurrentValue(): void {
+  showValue.value = false;
+}
 </script>
 
 <style scoped>
-.slider-component .slider-container {
-	width: 100%;
+/*********** Baseline, reset styles ***********/
+.current-value {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 10px;
 }
 
-.slider-component .slider-container .slider {
-	-webkit-appearance: none;
-	appearance: none;
-	width: 100%;
-	height: 4px;
-	border-radius: 2px;
-	background: #deecff;
-	outline: none;
-	opacity: 0.7;
-	-webkit-transition: .2s;
-	transition: opacity .2s;
+.slider-component .slider {
+  -webkit-appearance: none;
+  appearance: none;
+  background: transparent;
+  cursor: pointer;
+  width: 100%;
 }
 
-.slider-component .slider-container .slider:hover {
-	opacity: 1;
+/* Removes default focus */
+.slider-component .slider:focus {
+  outline: none;
 }
 
-.slider-component .slider-container .slider::-webkit-slider-thumb {
-	-webkit-appearance: none;
-	appearance: none;
-	width: 18px;
-	height: 18px;
-	background: #0e4791;
-	cursor: pointer;
-	border-radius: 50%;
+/******** Chrome, Safari, Opera and Edge Chromium styles ********/
+/* slider track */
+.slider-component .slider::-webkit-slider-runnable-track {
+  background-color: #deecff;
+  border-radius: 10px;
+  height: 4px;
 }
 
-.slider-component .slider-container .slider::-moz-range-thumb {
-	width: 18px;
-	height: 18px;
-	background: #D8A22E;
-	cursor: pointer;
-	border-radius: 50%;
+/* slider thumb */
+.slider-component .slider::-webkit-slider-thumb {
+  -webkit-appearance: none; /* Override default look */
+  appearance: none;
+  margin-top: -8px; /* Centers thumb on the track */
+  background-color: #0e4791;
+  border-radius: 10px;
+  height: 20px;
+  width: 10px;
+}
+
+.slider-component .slider:focus::-webkit-slider-thumb {
+  outline: 2px solid #0e4791;
+  outline-offset: 0.125rem;
+}
+
+/*********** Firefox styles ***********/
+/* slider track */
+.slider-component .slider::-moz-range-track {
+  background-color: #deecff;
+  border-radius: 10px;
+  height: 4px;
+}
+
+/* slider thumb */
+.slider-component .slider::-moz-range-thumb {
+  background-color: #0e4791;
+  border: none; /*Removes extra border that FF applies*/
+  border-radius: 10px;
+  height: 20px;
+  width: 10px;
+}
+
+.slider-component .slider:focus::-moz-range-thumb{
+  outline: 2px solid #0e4791;
 }
 </style>
