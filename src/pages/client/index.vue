623bbf8175fcf207510ed1f68329c97fcee5a52a
fravilpae <fravilpae@alum.us.es>
2024-03-28 21:19:02 +0100

    feat: Mostrar imagen de evento mejorado

--- src/pages/client/index.vue
+++ src/pages/client/index.vue
@@ -27,8 +27,12 @@
             @click="getDetailsEvent(event.id)">
             <div style=" cursor: pointer;margin:auto; text-align: center;display: flex; align-items: center; justify-content: left;">
               <img
+                v-if="event.image"
                 alt="Event"
-                :src="event.image?.image"
+                :src="event.image.image"
+                style="display: block; width: 30%; border-radius: 50%; max-width: 220px; max-height: 220px; min-width: 90px; min-height: 90px; margin: 16px;" />
+              <IMdiImageBrokenVariant
+                v-else
                 style="display: block; width: 30%; border-radius: 50%; max-width: 220px; max-height: 220px; min-width: 90px; min-height: 90px; margin: 16px;" />
               <div
                 class="ml-3"
