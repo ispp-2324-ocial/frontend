684ad59809e45ae0ce0e8c05bdf133fa359d58ab
jeszamgue <jeszamgue@alum.us.es>
2024-03-28 23:53:41 +0100

    chore: eliminacion 1 inncesario en validacion email

--- src/pages/login/register.vue
+++ src/pages/login/register.vue
@@ -68,7 +68,7 @@ const validationSchema = z.object({
   username: z.string().min(1, 'The name field is required'),
   password: z.string().min(8, 'Please enter a valid password'),
   password2: z.string().min(8, 'Please enter a valid password'),
-  email: z.string().email(1, 'Please enter a valid email')
+  email: z.string().email('Please enter a valid email')
 }).refine((data) => data.password === data.password2, {
   message: "Passwords don't match",
   path: ['password2']
