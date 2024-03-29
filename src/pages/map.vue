04e9e5276793abca041ba70d7dca8c01dcd3c09d
jeszamgue <jeszamgue@alum.us.es>
2024-03-29 20:37:51 +0100

    chore: eliminación prueba buscador + arreglos rebase

--- src/pages/map.vue
+++ src/pages/map.vue
@@ -1,11 +1,69 @@
 <template>
-  <LMap :markers="eventList" />
+  <form @submit.prevent="()=>{}">
+    <div
+      style="display:flex;
+    width: 100%;
+    justify-content: center;">
+      <BaseInput
+        v-model="firstName" />
+      <Boton
+        class="transparente"
+        @click="getEvents(firstName)">
+        <IIwwaSearch class="size-6 text-muted-foreground" />
+      </Boton>
+    </div>
+  </form>
+
+
+  <div v-if="contador > 0">
+    <LMap :markers="miArray" />
+  </div>
+  <div v-else>
+    <LMap :markers="eventList" />
+  </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 import { useEvent } from '@/composables/apis';
-import { EventApi } from '@/api';
+import { EventApi, type Event } from '@/api';
 
+
+const firstName = ref('');
 const { data: eventList } = await useEvent(EventApi, 'eventListList')();
+const miArray = ref<Event[]>([]);
+const contador = ref(0);
+
+
+/**
+ * Buscador por nombre, descripción o cliente
+ */
+function getEvents(text: string): void {
+  miArray.value.length = 0;
+  text = text.toLowerCase();
+  contador.value++;
+
+  for (const event of eventList.value) {
+
+    const name = event.name.toLowerCase();
+    const eventText = event.event.toLowerCase();
+    const ocialClient = String(event.ocialClient).toLowerCase();
+
+    if (name.includes(text) || eventText.includes(text) || ocialClient.includes(text)){
+      miArray.value.push(event);
+    }
+  }
+
+}
 
 </script>
+
+
+<style scoped>
+.transparente {
+  background-color: transparent;
+  border-color: transparent;
+  margin-left: 3%;
+}
+
+</style>
