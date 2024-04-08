7c9d8f08a723449452c5d13b5d9e535033cde26c
horgarler <horgarler@alum.us.es>
2024-04-08 05:18:33 +0200

    chore(Image): fixed lint warning

--- src/components/Layout/Views/ClientView.vue
+++ src/components/Layout/Views/ClientView.vue
@@ -78,22 +78,6 @@ async function getDetailsEvent(eventId: number | undefined) : Promise<void> {
 </script>
 
 <style scoped>
-.rect-img-container {
-  position: relative;
-}
-
-.rect-img-container::after {
-  content: "";
-  display: block;
-  padding-bottom: 100%;
-}
-
-.rect-img {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-}
 .nombre{
   font-size: clamp(15px, 6vw, 30px);
   font-weight: bold;
