5ae67141ad40b85a83522ae178682bfbdc107937
Fernando Fernández <ferferga@hotmail.com>
2024-04-08 11:57:25 +0000

    refactor(Selectcategory): use computed for vue-i18n reactivity
    Signed-off-by: GitHub <noreply@github.com>

--- src/components/Forms/SelectCategory.vue
+++ src/components/Forms/SelectCategory.vue
@@ -12,6 +12,7 @@
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
 import { useI18n } from 'vue-i18n';
 import { CategoryEnum } from '@/api';
 
@@ -24,11 +25,11 @@ interface CategoryOption {
   label: string;
 }
 
-const categories: CategoryOption[] = [
+const categories: CategoryOption[] = computed(() => [
   { value: CategoryEnum.Sports, label: t('categoryDeporte') },
   { value: CategoryEnum.Music, label: t('categoryMusica') },
   { value: CategoryEnum.Markets, label: t('categoryMercado') },
   { value: CategoryEnum.RelaxActivities, label: t('categoryRelax') },
   { value: CategoryEnum.LiveConcert, label: t('categoryConcierto') }
-];
+]);
 </script>
