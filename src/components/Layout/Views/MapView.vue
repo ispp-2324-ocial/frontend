b5ba02e0ab8f597c6f9af0ce3ea40caae81b9ca9
Fernando Fernández <ferferga@hotmail.com>
2024-04-09 12:17:25 +0000

    refactor: extract common logic, lint & type errors
    Signed-off-by: GitHub <noreply@github.com>

--- src/components/Layout/Views/MapView.vue
+++ src/components/Layout/Views/MapView.vue
@@ -7,45 +7,23 @@
       <BaseInput
         v-model="firstName" />
       <Boton
-        class="transparente"
-        @click="getEvents(firstName)">
+        class="transparente">
         <IIwwaSearch class="size-6 text-muted-foreground" />
       </Boton>
     </div>
   </form>
   <div>
-    <LMap :markers="miArray.length > 0 ? miArray : eventList" />
+    <LMap :markers="eventList" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { shallowRef } from 'vue';
 import { useEvent } from '@/composables/apis';
-import { EventApi, type Event } from '@/api';
+import { EventApi } from '@/api';
 
-const firstName = ref('');
+const firstName = shallowRef('');
 const { data: eventList } = await useEvent(EventApi, 'eventListList')();
-const miArray = ref<Event[]>([]);
-
-
-/**
- * Buscador por nombre, descripción o cliente
- */
-function getEvents(text: string): void {
-  miArray.value.length = 0;
-  text = text.toLowerCase();
-
-  for (const event of eventList.value) {
-
-    const name = event.name.toLowerCase();
-    const eventText = event.event.toLowerCase();
-    const ocialClient = String(event.ocialClient).toLowerCase();
-
-    if (name.includes(text) || eventText.includes(text) || ocialClient.includes(text)){
-      miArray.value.push(event);
-    }
-  }
-}
 </script>
 
 <style scoped>
