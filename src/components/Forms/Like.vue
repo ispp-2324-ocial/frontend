c67a18ec810632ca4dd4b1ddd4a6297889b162e1
AitorRD <aitroddue@alum.us.es>
2024-04-10 12:38:52 +0200

    feat(like): counter and component added

--- src/components/Forms/Like.vue
+++ src/components/Forms/Like.vue
@@ -8,61 +8,81 @@
       v-model="value"
       type="checkbox"
       class="hidden" />
-    <svg
+    <IMdiHeartOutline
       v-if="!value"
-      class="heart-icon"
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 56 56">
-      <path
-        fill="#0e4791"
-        d="M4.727 20.64c0 9.985 8.367 19.805 21.562 28.243c.516.304 1.219.633 1.711.633s1.195-.328 1.688-.633c13.218-8.438 21.586-18.258 21.586-28.242c0-8.297-5.696-14.157-13.29-14.157c-4.359 0-7.875 2.063-9.984 5.227c-2.11-3.14-5.648-5.227-9.984-5.227c-7.594 0-13.29 5.86-13.29 14.157m3.773 0c0-6.234 4.031-10.382 9.469-10.382c4.406 0 6.914 2.742 8.437 5.086c.633.937 1.032 1.195 1.594 1.195c.563 0 .914-.281 1.594-1.195c1.593-2.297 4.054-5.086 8.437-5.086c5.438 0 9.469 4.148 9.469 10.383c0 8.718-9.211 18.117-19.031 24.632c-.235.164-.375.282-.469.282c-.094 0-.258-.117-.492-.282C17.71 38.758 8.5 29.36 8.5 20.641" />
-    </svg>
-    <svg
+      class="heart-outline" />
+    <IMdiHeart
       v-else
-      class="heart-icon"
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 56 56">
-      <path
-        fill="#0e4791"
-        d="M28 49.516c.492 0 1.195-.328 1.688-.633c13.289-8.367 21.586-18.258 21.586-28.242c0-8.297-5.696-14.157-13.29-14.157c-4.359 0-7.875 2.579-9.984 6.54c-2.11-3.938-5.648-6.54-9.984-6.54c-7.594 0-13.29 5.86-13.29 14.157c0 9.984 8.227 19.992 21.563 28.242c.516.304 1.219.633 1.711.633" />
-    </svg>
+      class="heart-outline" />
+    <p class="elemento">
+      {{ counter }}
+    </p>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
 import { useRoute } from 'vue-router/auto';
-import { useEvent } from '@/composables/apis';
-import { EventApi } from '@/api';
+import { useApi, useEvent } from '@/composables/apis';
+import { EventApi, UsersApi } from '@/api';
 
 const value = ref(false);
 const isHovered = ref(false);
 const route = useRoute('/details/[id]');
 
+const { data: likedEvents } = await useEvent(EventApi, 'eventLikeList')(() => ({
+  id: Number(route.params.id)
+}));
+
+const counter = ref(likedEvents.value ? likedEvents.value.length : 0);
+
+console.log(counter.value);
+console.log(likedEvents.value);
+
+const { data: user } = await useApi(UsersApi, 'usersUserGetRetrieve')();
+
+console.log(user.value);
+
+if(likedEvents.value != undefined){
+  for ( const e of likedEvents.value) {
+    if (e.user == user.value.id) {
+      value.value = true;
+      break;
+    }
+  }
+}
+
 /**
  * Función que añade el like cuando el corazón el pulsado
  */
 async function toggleLike(): Promise<void> {
+  if (value.value) {
+    counter.value--;
+  } else {
+    counter.value++;
+  }
+
   value.value = !value.value;
 
-  await value.value ? useEvent(EventApi, 'eventLikeCreate') : useEvent(EventApi, 'eventLikeDestroy');
+  await (value.value ? useEvent(EventApi, 'eventLikeCreate')(() => ({
+    like: {
+      event: 0,
+      user: 0
+    }, id: Number(route.params.id)
+  })) : useEvent(EventApi, 'eventLikeDestroy')(() => ({
+    id: Number(route.params.id)
+  })));
 };
 
-const { data: likedEvents } = await useEvent(EventApi, 'eventLikeList')(() => ({
-  'id': Number(route.params.id)
-}));
 
-console.log(likedEvents.value);
 </script>
 
 <style scoped>
 .checkbox-container {
-  display: inline-block;
-  margin-left: 1%;
+  flex-direction: row;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
 .hidden {
@@ -70,15 +90,21 @@ console.log(likedEvents.value);
   left: -9999px;
 }
 
-.heart-icon {
-  width: 30px;
-  height: 30px;
+.heart-outline {
+  width: 50px;
+  height: 50px;
   cursor: pointer;
-
+  color: var(--o-color-theme)
 }
 
-.heart-icon:hover {
+.heart-outline:hover {
   transform: scale(0.9);
   transition: transform 0.3s ease;
+}
+
+.elemento {
+  font-size: clamp(15px, 5.5vw, 30px);
+  font-weight: normal;
+  margin-left: 2%;
 }
 </style>
