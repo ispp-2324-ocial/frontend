f2a676560c14b8a8b7567cd0edbc57e4ac831f3a
carvarsou <carvarsou@alum.us.es>
2024-04-10 14:02:01 +0200

    refactor(filter): dropdown with radix-vue components

--- src/components/Forms/Dropdown.vue
+++ src/components/Forms/Dropdown.vue
@@ -1,59 +1,44 @@
 <template>
-  <div class="dropdown">
-    <p>{{ $t('Filtra por:') }}</p>
-    <select v-model="selectedOption">
-      <option
-        value=""
-        disabled
-        hidden>
-        {{ $t('Categoría') }}
-      </option>
-      <option
-        v-for="(option, index) in options"
-        :key="index"
-        :value="option.value">
-        {{ option.label }}
-      </option>
-    </select>
-    <span>{{ selectedOption || $t('Categoría') }}</span>
-  </div>
+  <RDropdownMenuRoot v-model:open="toggleState">
+    <RDropdownMenuTrigger>
+      <slot name="trigger" />
+      <RDropdownMenuContent
+        v-show="toggleState"
+        class="min-w-[220px] outline-none bg-white rounded-md p-[5px] shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),_0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)] will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade"
+        :side-offset="5">
+        <RDropdownMenuLabel> {{ $t('Filtra por:') }}</RDropdownMenuLabel>
+        <RDropdownMenuSeparator />
+        <RDropdownMenuItem>
+          <RDropdownMenuCheckboxItem v-model:checked="checkboxCategory">
+            {{ $t('Categoría') }}
+          </RDropdownMenuCheckboxItem>
+        </RDropdownMenuItem>
+        <RDropdownMenuItem>
+          <RDropdownMenuCheckboxItem v-model:checked="checkboxDate">
+            {{ $t('Fecha') }}
+          </RDropdownMenuCheckboxItem>
+        </RDropdownMenuItem>
+        <RDropdownMenuItem>
+          <RDropdownMenuCheckboxItem v-model:checked="checkboxDistance">
+            {{ $t('Distancia') }}
+          </RDropdownMenuCheckboxItem>
+        </RDropdownMenuItem>
+        <RDropdownMenuItem>
+          <RDropdownMenuCheckboxItem v-model:checked="checkboxLikes">
+            {{ $t('Likes') }}
+          </RDropdownMenuCheckboxItem>
+        </RDropdownMenuItem>
+      </RDropdownMenuContent>
+    </RDropdownMenuTrigger>
+  </RDropdownMenuRoot>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { shallowRef } from 'vue';
 
-const selectedOption = ref('option1'); // Set default option value here
-const options = ref([
-  { label: 'Categoría', value: 'option1' },
-  { label: 'Fecha', value: 'option2' },
-  { label: 'Distancia', value: 'option3' }
-]);
+const toggleState = defineModel<boolean>();
+const checkboxCategory = shallowRef(false);
+const checkboxDate = shallowRef(false);
+const checkboxDistance = shallowRef(false);
+const checkboxLikes = shallowRef(false);
 </script>
-
-<style scoped>
-.dropdown {
-  position: relative;
-}
-
-.dropdown select {
-  background-color: #0e4791;
-  color: white;
-  padding: 8px 12px;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-}
-
-.dropdown select:focus {
-  outline: none;
-}
-
-.dropdown select::-ms-expand {
-  display: none;
-}
-
-span {
-  position: absolute;
-  left: 10%;
-}
-</style>
