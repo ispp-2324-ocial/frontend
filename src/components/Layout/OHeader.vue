f38f23f9430e6c9bf21595da3e981453f6c841d1
Fernando Fernández <ferferga@hotmail.com>
2024-04-11 13:59:01 +0200

    refactor: use a full flexbox layout
    Signed-off-by: Fernando Fernández <ferferga@hotmail.com>

---
+++ src/components/Layout/OHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <header class="header flex">
+    <slot />
+  </header>
+</template>
+
+<style scoped>
+.header {
+  top: 0px;
+  z-index: 1008;
+  transform: translateY(0%);
+  position: fixed;
+  left: 0px;
+  width: calc(100% + 0px);
+  border: 0;
+  flex-direction: column;
+}
+</style>
