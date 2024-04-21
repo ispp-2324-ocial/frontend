781919b0edeef12bc6f77d5897cf3f981846b7f2
carvarsou <carvarsou@alum.us.es>
2024-04-21 20:44:15 +0200

    fix(search): added search logic again

--- src/components/Layout/Views/MapView.vue
+++ src/components/Layout/Views/MapView.vue
@@ -3,9 +3,10 @@
     <form @submit.prevent="()=>{}">
       <div
         class="flex justify-center">
-        <BaseInput />
+        <BaseInput v-model="search" />
         <Boton
-          class="transparent p-1">
+          class="transparent p-1"
+          @click="getEvents(search)">
           <IMdiSearch class="size-6" />
         </Boton>
         <Dropdown>
@@ -25,15 +26,41 @@
   </OHeader>
   <div class="h-full">
     <LMap
-      :markers="eventList" />
+      :markers="miArray.length>0 ? miArray : eventList" />
+    <p>{{ console.log(miArray) }}</p>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 import { useEvent } from '@/composables/apis';
-import { EventApi } from '@/api';
+import { EventApi, type Event } from '@/api';
 
 const { data: eventList } = await useEvent(EventApi, 'eventList')();
+
+const search = ref('');
+const miArray = ref<Event[]>([]);
+
+/**
+ * Buscador por nombre, descripción o cliente
+ *
+ */
+function getEvents(text: string): void {
+  miArray.value.length = 0;
+  text = text.toLowerCase();
+
+  for (const event of eventList.value) {
+    const name = event.name.toLowerCase();
+    const eventText = event.description.toLowerCase();
+    const creator = event.creator.name.toLowerCase();
+
+    if (name.includes(text) || eventText.includes(text) || creator.includes(text)){
+      miArray.value.push(event);
+
+    }
+  }
+}
+
 </script>
 
 <style scoped>
