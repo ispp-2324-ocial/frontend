8c98b145dd005723aa8a94e4f3ad1f85edf73d33
carvarsou <carvarsou@alum.us.es>
2024-04-22 01:17:54 +0200

    style(filter): css styles

--- src/components/Forms/Dropdown.vue
+++ src/components/Forms/Dropdown.vue
@@ -11,33 +11,51 @@
         <RDropdownMenuLabel> {{ $t('Filtra por:') }}</RDropdownMenuLabel>
         <RDropdownMenuSeparator />
         <div>
-          <div class="flex align-center font-bold">
+          <div class="title">
             {{ $t('Categoría') }}
           </div>
           <SelectCategory
             v-model="form.category"
-            class="border ml-2"
+            class="selector"
             @click.stop />
         </div>
         <div>
-          <div class="font-bold justify-center">
+          <div class="title">
             {{ $t('Fecha') }}
           </div>
-          <BaseInput 
-            v-model="form.date"
-            tipo="datetime-local"
-            @click.stop />
+          <div class="flex row" 
+               style="align-items: center;">
+            <div class="justify-left col-3 date-left">
+              {{ $t('Desde') }}
+            </div>
+            <BaseInput
+              v-model="form.time_start"
+              class="ml-2 justify-right col-9 date-right"
+              tipo="datetime-local"
+              @click.stop />
+          </div>
+          <div class="flex row mt-2" 
+               style="align-items: center;">
+            <div class="justify-left col-3 date-left">
+              {{ $t('Hasta') }}
+            </div>
+            <BaseInput
+              v-model="form.time_end"
+              class="ml-2 justify-right col-9 date-right"
+              tipo="datetime-local"
+              @click.stop />
+          </div>
         </div>
         <div>
-          <div class="font-bold justify-center">
+          <div class="title">
           {{ $t('Distancia') }}
           </div>
-          <Slider 
+          <Slider
             v-model="form.distance"
             @click.stop />
         </div>
         <div>
-          <div class="font-bold justify-center">
+          <div class="title">
             {{ $t('Likes') }}
           </div>
           <Checkbox 
@@ -45,7 +63,7 @@
             @click.stop />
         </div>
         <div>
-          <div class="font-bold justify-center">
+          <div class="title">
           {{ $t('Destacados') }}
           </div>
           <Checkbox
@@ -69,7 +87,8 @@ const toggleState = ref<boolean>();
 
 export interface ConfigurationParameters{
   category?: CategoryEnum;
-  date?: string;
+  time_start?: string;
+  time_end?: string;
   distance?: string;
   likes: boolean;
   highlighted: boolean;
@@ -78,7 +97,8 @@ export interface ConfigurationParameters{
 const current_date = new Date();
 const form = ref({
   category: undefined,
-  date: new Date(current_date.getTime() - (current_date.getTimezoneOffset() * 60_000)).toJSON().slice(0,16),
+  time_start: new Date(current_date.getTime() - (current_date.getTimezoneOffset() * 60_000)).toJSON().slice(0,16),
+  time_end: new Date(current_date.getTime() - (current_date.getTimezoneOffset() * 60_000)).toJSON().slice(0,16),
   distance: undefined,
   likes: false,
   highlighted: false
@@ -91,3 +111,32 @@ watch(form, () => {
 }
 );
 </script>
+
+<style>
+.title{
+  font-weight: bold;
+  justify-content: center;
+  margin: 1vh 0 1vh;
+}
+.selector{
+  border: solid 1px #0e4791;
+  background-color: rgb(255, 255, 255);
+  border-radius: 25px;
+  margin: auto;
+  height: 6vh;
+  justify-content: center;
+  padding-left: 1vw;
+  display: flex;
+  width: 80%;
+}
+.date-left{
+  width: 90%;
+  margin-left: 1vw;
+  justify-content: center;
+}
+.date-right{
+  width: 90%;
+  margin-right: 1vw;
+  justify-content: center;
+} 
+</style>
