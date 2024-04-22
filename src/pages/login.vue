234a6e135081cf79c8c14eda55cb0d5c4eb27a25
Fernando Fernández <ferferga@hotmail.com>
2024-04-22 15:41:32 +0000

    feat(error-toasts): localisable unknown error message
    Signed-off-by: GitHub <noreply@github.com>

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -101,7 +101,9 @@ async function Login() : Promise<void> {
       if ('error' in casted) {
         useToast(casted.error, 'error');
       } else {
-        useToast('Error desconocido', 'error');
+        const { t } = useI18n();
+
+        useToast(t('Error desconocido'), 'error');
       }
     }
   } else {
@@ -138,7 +140,9 @@ const callback = async (googleresponse : GoogleResponseObject): Promise<void> =>
     if ('error' in casted) {
       useToast(casted.error, 'error');
     } else {
-      useToast('Error desconocido', 'error');
+      const { t } = useI18n();
+
+      useToast(t('Error desconocido'), 'error');
     }
   }
 };
