14ede7197abf090c9f8fe77cf16108e4e987b2fa
Fernando Fernández <ferferga@hotmail.com>
2024-04-22 11:18:11 +0200

    refactor: address review comments

--- src/pages/client/events/create.vue
+++ src/pages/client/events/create.vue
@@ -101,8 +101,8 @@ import { toBase64 } from '@/utils/data-manipulation';
 
 const { t } = useI18n();
 
-const { data : currentSubscription } = await useApi(SubscriptionApi, 'subscriptionRetrieve')(() => ({}));
-const subsEnum = [TypeSubscriptionEnum.Free, TypeSubscriptionEnum.Basic, TypeSubscriptionEnum.Pro ];
+const { data : currentSubscription } = await useApi(SubscriptionApi, 'subscriptionRetrieve')();
+const subsEnum = [TypeSubscriptionEnum.Free, TypeSubscriptionEnum.Basic, TypeSubscriptionEnum.Pro];
 
 
 const validationSchema = z.object({
