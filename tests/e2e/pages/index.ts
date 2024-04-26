ab2e10502e096306999d65694df9af191bbbfacd
Domi-ATLAS <edupriv2002@gmail.com>
2024-04-26 12:38:02 +0200

    feat(test): intento de primer test, da error en el getByPlaceholder, pero al inspeccionar la pagina el atributo es de ese tipo

--- tests/e2e/pages/index.ts
+++ tests/e2e/pages/index.ts
@@ -9,3 +9,25 @@ test('Se muestran errores al hacer click en el botón sin contenido alguno', asy
 
   expect(el).toBeDefined();
 });
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('http://localhost:3000/#/login');
+});
+
+
+test.describe('Testing del login y register', () =>{
+
+
+  test('Se registra correctamente', async ({ page }) => {
+
+
+    await page.goto('http://localhost:3000/#/register');
+    await page.getByPlaceholder('Nombre de usuario').fill('ocial12345');
+    await page.getByPlaceholder('Correo electrónico').fill('edupizlop@gmail.com');
+    await page.getByPlaceholder('Contraseña').fill('Baloncesto02');
+    await page.getByPlaceholder('Confirmar contraseña').fill('Baloncesto02');
+
+    await page.getByRole('checkbox').click();
+    await page.getByRole('button').click();
+  });
+});
