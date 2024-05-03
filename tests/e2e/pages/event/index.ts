7019b73f9a153152d31b090353dd6978423084df
Domi-ATLAS <edupriv2002@gmail.com>
2024-05-03 12:23:50 +0200

    feat(tests): tests de evento y filtrado comenzados

--- tests/e2e/pages/event/index.ts
+++ tests/e2e/pages/event/index.ts
@@ -25,9 +25,123 @@ test.describe('Testing para crear eventos', () =>{
 
     await page.waitForTimeout(5000);
 
+    expect(page.url()).toBe('http://localhost:3000/#/client/events/create');
+
     const url = page.url();
 
     expect(url).toBe('http://localhost:3000/#/');
 
   });
 });
+
+
+test.describe('Testing para darle like a eventos', () =>{
+  test('Se registra correctamente', async ({ page }) => {
+    await page.goto('http://localhost:3000/#/login');
+    await page.getByPlaceholder('Username', { exact: true }).fill('ocial1234578');
+    await page.getByPlaceholder('Password', { exact: true }).fill('Baloncesto02');
+    await page.getByRole('button', { name: 'Log in' }).click();
+
+    await page.waitForTimeout(5000);
+
+    await page.goto('http://localhost:3000/#/profile');
+
+    const h1Text = await page.$eval('h1', element => element.textContent);
+
+    expect(h1Text).toBe('ocial1234578');
+
+    await page.goto('http://localhost:3000/#/');
+
+    await page.getByRole('button', { name: 'Look details' }).click();
+
+    await page.waitForTimeout(5000);
+
+    expect(page.url()).toBe('http://localhost:3000/#/details/45');
+
+    await page.getByRole('button', { name: 'Like' }).click();
+
+    await page.waitForTimeout(5000);
+
+    await page.goto('http://localhost:3000/#/event');
+
+    await page.waitForTimeout(5000);
+
+    const text = await page.textContent('body');
+
+    expect(text).toContain('New SaiVintage al estilo Sevillano');
+
+
+  });
+});
+
+test.describe('Testing para los filtros', () =>{
+  test('Se registra correctamente', async ({ page }) => {
+    await page.goto('http://localhost:3000/#/login');
+    await page.getByPlaceholder('Username', { exact: true }).fill('ocial1234578');
+    await page.getByPlaceholder('Password', { exact: true }).fill('Baloncesto02');
+    await page.getByRole('button', { name: 'Log in' }).click();
+
+    await page.waitForTimeout(5000);
+
+    await page.goto('http://localhost:3000/#/profile');
+
+    const h1Text = await page.$eval('h1', element => element.textContent);
+
+    expect(h1Text).toBe('ocial1234578');
+
+    await page.goto('http://localhost:3000/#/');
+
+    await page.getByRole('button', { name: 'Filter' }).click();
+
+    await page.click('.select-data');
+
+    await page.waitForTimeout(1000);
+
+    await page.click('.option[data-value="Sport"]');
+
+    //Aqui quiero encontrar la lista de eventos visibles y que solo esten aquellos con atributo categoria=Sport
+    await page.getByRole('button', { name: 'Filter' }).click();
+
+    await page.click('.select-data');
+
+    await page.waitForTimeout(1000);
+
+    await page.click('.option[data-value="Music"]');
+    //Aqui quiero encontrar la lista de eventos visibles y que solo esten aquellos con atributo categoria=Music
+    await page.getByRole('button', { name: 'Filter' }).click();
+
+    await page.click('.select-data');
+
+    await page.waitForTimeout(1000);
+
+    await page.click('.option[data-value="Market"]');
+    //Aqui quiero encontrar la lista de eventos visibles y que solo esten aquellos con atributo categoria=Market
+    await page.getByRole('button', { name: 'Filter' }).click();
+
+    await page.click('.select-data');
+
+    await page.waitForTimeout(1000);
+
+    await page.click('.option[data-value="Relaxing activities"]');
+    //Aqui quiero encontrar la lista de eventos visibles y que solo esten aquellos con atributo categoria=Relaxing activities
+    await page.getByRole('button', { name: 'Filter' }).click();
+
+    await page.click('.select-data');
+
+    await page.waitForTimeout(1000);
+
+    await page.click('.option[data-value="Concert"]');
+    //Aqui quiero encontrar la lista de eventos visibles y que solo esten aquellos con atributo categoria=Concert
+
+    await page.getByRole('button', { name: 'Filter' }).click();
+
+    await page.click('.select-data');
+
+    await page.waitForTimeout(1000);
+
+    await page.getByRole('checkbox').click();
+    //Aqui quiero encontrar la lista de eventos visibles y que solo esten aquellos que esten destacados
+
+  });
+});
+
